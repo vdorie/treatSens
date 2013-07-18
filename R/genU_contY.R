4d92d9b37fd2862dfddb2c993c07b9d7b86c540f
Masataka Harada <masatakaharada@gmail.com>
2013-07-18 03:38:28 +0000

    in genU_contY.R,   nx = dim(X)[2]-->  nx = dim(x)[2]

--- R/genU_contY.R
+++ R/genU_contY.R
@@ -52,7 +52,7 @@ contYZbinaryU <- function(y, z, cy, cz, vy, vz, theta) { #Y.res, Z.res, rY, rZ,v
 
 contYbinaryZU <- function(y, z, x, cy, cz, theta) { 
   n = length(y)
-  nx = dim(X)[2]
+  nx = dim(x)[2]
   null.resp = lm(y~z+x)
   null.trt = glm(z~x, family = binomial(link ="probit"))
   v_Y = var(null.resp$resid)*(n-1)/(n-nx-2)
@@ -60,14 +60,22 @@ contYbinaryZU <- function(y, z, x, cy, cz, theta) {
   
   p = 0.5
   
-  for(j in 1:10) {
+  for(j in 1:5) {
     U = rbinom(n,1,p)
     
-    U.fit = lm(y~z+x+U)
-    y.coef = U.fit$coef
-    y.coef[length(y.coef)]  = cy
-    z.coef = glm(z~x+U, family=binomial(link="probit"))$coef
-    z.coef[length(z.coef)] = cz
+    if (F) {  #original code
+      U.fit = lm(y~z+x+U)
+      y.coef = U.fit$coef
+      y.coef[length(y.coef)]  = cy
+      z.coef = glm(z~x+U, family=binomial(link="probit"))$coef
+      z.coef[length(z.coef)] = cz
+      v_Y = var(U.fit$resid)*(n-1)/(n-nx-2)
+    }
+    
+    #MH: New codes that uses glm + offset.
+    U.fit = lm(y~z+x, offset=cy*U)
+    y.coef = c(U.fit$coef, cy)
+    z.coef = c(glm(z~x, family=binomial(link="probit"), offset=cz*U)$coef, cz)
     v_Y = var(U.fit$resid)*(n-1)/(n-nx-2)
     
     pyzu = dnorm(y-cbind(1,z,x,1)%*%matrix(y.coef, ncol = 1), 0, sqrt(v_Y))* 
@@ -86,3 +94,6 @@ contYbinaryZU <- function(y, z, x, cy, cz, theta) {
   U = rbinom(n,1,p)
   return(U)
 }
+
+
+
