f729b943264cda0896b068d9b65c2635de13debe
Vincent Dorie <vdorie@gmail.com>
2019-03-20 23:01:28 -0400

    parity with dbarts release candidate

--- src/misc/simd.c
+++ src/misc/simd.c
@@ -47,24 +47,36 @@ misc_simd_instructionLevel misc_simd_getMaxSIMDInstructionSet() {
   return flag == 0 ? MISC_INST_C : (misc_simd_instructionLevel) (8 * sizeof(unsigned int) - countLeadingZeros(flag));
 }
 
-// function pointers
+/* function pointers */
+// linear algebra
 extern void (*misc_addVectors)(const double* restrict x, misc_size_t length, double alpha, const double* restrict y, double* restrict z);
 extern void (*misc_setVectorToConstant)(double* x, size_t length, double alpha);
 
-// implementing functions
-extern void misc_addVectors_c(const double* restrict x, size_t length, double alpha, const double* restrict y, double* restrict z);
-extern void misc_addVectors_sse2(const double* restrict x, size_t length, double alpha, const double* restrict y, double* restrict z);
-extern void misc_addVectors_avx(const double* restrict x, size_t length, double alpha, const double* restrict y, double* restrict z);
-extern void misc_setVectorToConstant_c(double* x, size_t length, double alpha);
-extern void misc_setVectorToConstant_sse2(double* x, size_t length, double alpha);
-extern void misc_setVectorToConstant_avx(double* x, size_t length, double alpha);
 
-extern void misc_initStats(misc_simd_instructionLevel i);
-extern void misc_initART(misc_simd_instructionLevel i);
+/* implementing functions */
+#ifdef HAVE_AVX
+extern void misc_addVectors_avx(const double* restrict x, size_t length, double alpha, const double* restrict y, double* restrict z);
+extern void misc_setVectorToConstant_avx(double* x, size_t length, double alpha);
+#endif
+
+#ifdef HAVE_SSE2
+extern void misc_addVectors_sse2(const double* restrict x, size_t length, double alpha, const double* restrict y, double* restrict z);
+extern void misc_setVectorToConstant_sse2(double* x, size_t length, double alpha);
+#endif
+
+extern void misc_addVectors_c(const double* restrict x, size_t length, double alpha, const double* restrict y, double* restrict z);
+extern void misc_setVectorToConstant_c(double* x, size_t length, double alpha);
 
 void misc_simd_init(void) {
-  int flag = getx86SIMDSets();
+  misc_simd_instructionLevel i = misc_simd_getMaxSIMDInstructionSet();
   
+  misc_simd_setSIMDInstructionSet(i);
+}
+
+extern void misc_stat_setSIMDInstructionSet(misc_simd_instructionLevel i);
+
+void misc_simd_setSIMDInstructionSet(misc_simd_instructionLevel i)
+{
   // if the compiler supports SIMD instruction sets then the binary will be built
   // with code that supports them; check this by preprocessor defines
   
@@ -73,81 +85,26 @@ void misc_simd_init(void) {
   
   // default to base C implementations
   
-#ifdef __AVX2__
-  if (flag & SIMD_AVX2) {
-    misc_addVectors = &misc_addVectors_avx;
-    misc_setVectorToConstant = &misc_setVectorToConstant_avx;
-  } else 
-#endif
-#ifdef __AVX__
-  if (flag & SIMD_AVX) {
-    misc_addVectors = &misc_addVectors_avx;
-    misc_setVectorToConstant = &misc_setVectorToConstant_avx;
-  } else 
-#endif
-#ifdef __SSE4_1__
-  if (flag & SIMD_SSE4_1) {
-    misc_addVectors = &misc_addVectors_sse2;
-    misc_setVectorToConstant = &misc_setVectorToConstant_sse2;
-  } else 
-#endif
-#ifdef __SSE2__
-  if (flag & SIMD_SSE2) {
-    misc_addVectors = &misc_addVectors_sse2;
-    misc_setVectorToConstant = &misc_setVectorToConstant_sse2;
-  } else
-#endif
-  {
-    misc_addVectors = &misc_addVectors_c;
-    misc_setVectorToConstant = &misc_setVectorToConstant_c;
-  }
-  
-  misc_simd_instructionLevel i = flag == 0 ? MISC_INST_C : (misc_simd_instructionLevel) (8 * sizeof(unsigned int) - countLeadingZeros(flag));
-  
-  misc_initStats(i);
-}
-
-// #include <external/io.h>
-
-void misc_simd_setSIMDInstructionSet(misc_simd_instructionLevel i)
-{
   if (i < MISC_INST_C || i > MISC_INST_AVX512F) return;
   
-#ifdef __AVX2__
-  if (i >= MISC_INST_AVX2) {
-    // ext_printf("setting SIMD level to AVX2\n");
-    misc_addVectors = &misc_addVectors_avx;
-    misc_setVectorToConstant = &misc_setVectorToConstant_avx;
-  } else
-#endif
-#ifdef __AVX__
+#ifdef HAVE_AVX
   if (i >= MISC_INST_AVX) {
-    // ext_printf("setting SIMD level to AVX\n");
     misc_addVectors = &misc_addVectors_avx;
     misc_setVectorToConstant = &misc_setVectorToConstant_avx;
-  } else
+  } else 
 #endif
-#ifdef __SSE4_1__
-  if (i >= MISC_INST_SSE4_1) {
-    // ext_printf("setting SIMD level to SSE4_1\n");
-    misc_addVectors = &misc_addVectors_sse2;
-    misc_setVectorToConstant = &misc_setVectorToConstant_sse2;
-  } else
-#endif
-#ifdef __SSE2__
+#ifdef HAVE_SSE2
   if (i >= MISC_INST_SSE2) {
-    // ext_printf("setting SIMD level to SSE2\n");
     misc_addVectors = &misc_addVectors_sse2;
     misc_setVectorToConstant = &misc_setVectorToConstant_sse2;
   } else
 #endif
   {
-    // ext_printf("setting SIMD level to C\n");
     misc_addVectors = &misc_addVectors_c;
     misc_setVectorToConstant = &misc_setVectorToConstant_c;
   }
   
-  misc_initStats(i);
+  misc_stat_setSIMDInstructionSet(i);
 }
 
 
